8c95fc89056d76319bf9bc57d66ccd2a2f7b848f
tushar-nanda <109102607+tushar-nanda@users.noreply.github.com>
2025-12-18 16:22:37 +0530

    8

--- camel-microservice-a/src/main/java/com/in28min/microservice/camel_microservice_a/routes/b/MyFileRouter.java
+++ camel-microservice-a/src/main/java/com/in28min/microservice/camel_microservice_a/routes/b/MyFileRouter.java
@@ -4,7 +4,7 @@ import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
 
-@Component
+//@Component
 public class MyFileRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
---
+++ camel-microservice-a/src/main/java/com/in28min/microservice/camel_microservice_a/routes/c/ActiveMqSenderRouter.java
@@ -0,0 +1,16 @@
+package com.in28min.microservice.camel_microservice_a.routes.c;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ActiveMqSenderRouter extends RouteBuilder {
+
+    @Override
+    public void configure() {
+
+        from("timer:active-mq-timer?period=10000")
+                .transform().constant("My message for ActiveMQ")
+                .to("activemq:queue:my-activemq-queue");
+    }
+}
