5612bd7a5ce064ada94dc7c04bfec187fc7ff574
tushar-nanda <109102607+tushar-nanda@users.noreply.github.com>
2025-12-18 16:44:46 +0530

    10

--- camel-microservice-b/src/main/java/com/in28min/microservice/camel_microservice_b/routes/ActiveMqReceiverRouter.java
+++ camel-microservice-b/src/main/java/com/in28min/microservice/camel_microservice_b/routes/ActiveMqReceiverRouter.java
@@ -1,6 +1,8 @@
 package com.in28min.microservice.camel_microservice_b.routes;
 
+import com.in28min.microservice.camel_microservice_b.CurrencyExchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,6 +11,7 @@ public class ActiveMqReceiverRouter extends RouteBuilder {
     public void configure() throws Exception {
 
         from("activemq:my-activemq-queue")
+                .unmarshal().json(JsonLibrary.Jackson , CurrencyExchange.class)
                 .to("log:received-message-from-active-mq");
 
 
