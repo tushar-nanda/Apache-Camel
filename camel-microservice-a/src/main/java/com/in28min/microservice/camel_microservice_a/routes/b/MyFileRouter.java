4c14e9cc994318524f0e3a0f79840f6f00458097
tushar-nanda <109102607+tushar-nanda@users.noreply.github.com>
2025-12-19 00:04:44 +0530

    14

--- camel-microservice-a/src/main/java/com/in28min/microservice/camel_microservice_a/routes/b/MyFileRouter.java
+++ camel-microservice-a/src/main/java/com/in28min/microservice/camel_microservice_a/routes/b/MyFileRouter.java
@@ -4,13 +4,21 @@ import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
 
-//@Component
+@Component
 public class MyFileRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
         from("file:file/input")
-                .log("${body}")
+                .routeId("Files-Input-Route")
+                .transform().body(String.class)
+                .choice()
+                    .when(simple("${file:ext} ends with 'xml'"))
+                    .log("XML file")
+                    .when(simple("${body} contains 'USD'"))
+                    .log("not a xml but has USD")
+                .otherwise()
+                .log("not an xml file")
                 .to("file:file/output");
     }
 }
