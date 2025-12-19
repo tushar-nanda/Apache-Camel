02eadeca8f3bddb94bf997cebe1f9aa0287690ed
tushar-nanda <109102607+tushar-nanda@users.noreply.github.com>
2025-12-19 12:48:30 +0530

    23

--- camel-microservice-a/src/main/java/com/in28min/microservice/camel_microservice_a/routes/patterns/EipPatternsRouter.java
+++ camel-microservice-a/src/main/java/com/in28min/microservice/camel_microservice_a/routes/patterns/EipPatternsRouter.java
@@ -1,17 +1,26 @@
 package com.in28min.microservice.camel_microservice_a.routes.patterns;
 
+import org.apache.camel.Body;
+import org.apache.camel.ExchangeProperties;
+import org.apache.camel.Headers;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class EipPatternsRouter extends RouteBuilder {
 
     @Autowired
     SplitterComponent splitterComponent;
+
+    @Autowired
+    DynamicRouterBean dynamicRouterBean;
     @Override
     public void configure() throws Exception {
 
@@ -44,18 +53,70 @@ public class EipPatternsRouter extends RouteBuilder {
 //                .to("log:aggregate-json");
 
         // routing slip pattern
-        String routingSlip = "direct:endpoint1,direct:endpoint2";
+//        String routingSlip = "direct:endpoint1,direct:endpoint2";
+//
+//        from("timer:routingSlip?period=10000")
+//                .transform().constant("My message is hardcoded")
+//                .routingSlip(simple(routingSlip));
+//
+//        from("direct:endpoint1")
+//                .to("log:Hi directpoint1");
+//        from("direct:endpoint2")
+//                .to("log:Hi directpoint2");
+//        from("direct:endpoint3")
+//                .to("log:Hi directpoint3");
 
-        from("timer:routingSlip?period=10000")
-                .transform().constant("My message is hardcoded")
-                .routingSlip(simple(routingSlip));
+            //dynamic routing
+        //Dynamic Routing
+
+        //Step 1, Step 2, Step 3
+
+        from("timer:dynamicRouting?period={{timePeriod}}")
+                .transform().constant("My Message is Hardcoded")
+                .dynamicRouter(method(dynamicRouterBean));
+
+
+        //Endpoint1
+        //Endpoint2
+        //Endpoint3
+
 
         from("direct:endpoint1")
-                .to("log:Hi directpoint1");
+                .wireTap("log:wire-tap") //add
+                .to("{{endpoint-for-logging}}");
+
         from("direct:endpoint2")
-                .to("log:Hi directpoint2");
+                .to("log:directendpoint2");
+
         from("direct:endpoint3")
-                .to("log:Hi directpoint3");
+                .to("log:directendpoint3");
+    }
+}
+@Component
+class DynamicRouterBean{
+
+    private Logger logger = LoggerFactory.getLogger(DynamicRouterBean.class);
+
+    int invocations ;
+
+    public String decideTheNextEndpoint(
+            @ExchangeProperties Map<String, String> properties,
+            @Headers Map<String, String> headers,
+            @Body String body
+    ) {
+        logger.info("{} {} {}", properties, headers, body);
+
+        invocations++;
+
+        if(invocations%3==0)
+            return "direct:endpoint1";
+
+        if(invocations%3==1)
+            return "direct:endpoint2,direct:endpoint3";
+
+        return null;
+
+
     }
 }
 
