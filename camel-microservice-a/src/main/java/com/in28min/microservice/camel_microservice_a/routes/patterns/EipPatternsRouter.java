e54e28e54fb5e32a18e144012cf7969eb924587b
tushar-nanda <109102607+tushar-nanda@users.noreply.github.com>
2025-12-19 11:42:18 +0530

    20

--- camel-microservice-a/src/main/java/com/in28min/microservice/camel_microservice_a/routes/patterns/EipPatternsRouter.java
+++ camel-microservice-a/src/main/java/com/in28min/microservice/camel_microservice_a/routes/patterns/EipPatternsRouter.java
@@ -1,11 +1,16 @@
 package com.in28min.microservice.camel_microservice_a.routes.patterns;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class EipPatternsRouter extends RouteBuilder {
 
+    @Autowired
+    SplitterComponent splitterComponent;
     @Override
     public void configure() throws Exception {
 
@@ -16,12 +21,28 @@ public class EipPatternsRouter extends RouteBuilder {
 //
 
         // how we can process the file line by line
-        from("file:file/csv")
-                .unmarshal().csv()
-                .split(body())
-                .log("data sending to the split-queue")
-                .to("activemq:split-queue")
-                .end();
+//        from("file:file/csv")
+//                .unmarshal().csv()
+//                .split(body())
+//                .log("data sending to the split-queue")
+//                .to("activemq:split-queue")
+//                .end();
 
+        //splting based on bean
+        from("file:file/csv")
+                .convertBodyTo(String.class)
+                .split(method(splitterComponent))
+                .to("activemq:split-queue");
+
+    }
+}
+
+@Component
+class SplitterComponent
+{
+    public List<String> splitInput(String body)
+    {
+
+        return List.of("AVC" , "AFE");
     }
 }
